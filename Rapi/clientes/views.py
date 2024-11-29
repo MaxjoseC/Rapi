7f115555c27e6646af193ce3b5b4742f34e91da2
MaxjoseC <Maxjose17@gmail.com>
2024-11-29 12:37:57 -0400

    filtro de clientes

--- Rapi/clientes/views.py
+++ Rapi/clientes/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Cliente
 from .serializers import ClienteSerializers
+from django_filters.rest_framework import DjangoFilterBackend
+from .filter import ClienteFilter
 
 # Creación de views.
 
@@ -9,8 +11,10 @@ class ClienteListcreate(generics.ListCreateAPIView):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializers
 
-class ClienteRetrieveUpdateDestroy(generics.RetrieveUpdateAPIView):
-    queryet = Cliente.objects.all()
-    serializer_class = ClienteSerializers
 
+class ClienteRetrieveUpdateDestroy(generics.RetrieveUpdateAPIView):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializers
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ClienteFilter
 
